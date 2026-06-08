26e291ff0a6d12aa328a07c2497001f51557fe02
shabeeh haider <shabeehhaider93@gmail.com>
2026-06-08 20:50:56 +0500

    due date edit issue

--- nextgen-ticketing/frontend/src/pages/tickets/components/TicketDetailModal.tsx
+++ nextgen-ticketing/frontend/src/pages/tickets/components/TicketDetailModal.tsx
@@ -47,6 +47,7 @@ const TicketDetailModal: React.FC<TicketDetailModalProps> = ({
     ticket?.status?.name === StatusName.CLOSED;
 
   const { user } = useAuth();
+  const isClient = user?.role?.name === RoleName.CUSTOMER;
   const { showNotification, setIsLoading } = useNotification();
   const navigate = useNavigate();
   const { control, handleSubmit, reset, watch, setValue } =
@@ -815,7 +816,7 @@ const TicketDetailModal: React.FC<TicketDetailModalProps> = ({
                           <span>Due Date</span>
                         </div>
                       }
-                      disabled={!canUpdate || isDisbaledMode}
+                      disabled={!canUpdate || isDisbaledMode || isClient }
                     />
 
                     {/* Assignment */}
